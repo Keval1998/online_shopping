ddfdd7b9d205b5a4357305a6a695a2e66758b4b6
KEVAL <KEVAL@RK07>
2018-01-13 18:46:36 +0530

    Add project files.

---
+++ online_shopping/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(online_shopping.Startup))]
+namespace online_shopping
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
